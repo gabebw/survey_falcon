9a6498b4932c234a51586fb654434b24bc0b151f
Gabe Berke-Williams <gabebw@gabebw.com>
2014-11-15 18:41:14 -0500

    Create TextAnswer/MultiChoiceAnswer as appropriate

--- app/models/custom_survey.rb
+++ app/models/custom_survey.rb
@@ -8,7 +8,18 @@ class CustomSurvey
   def questions_attributes=(attributes)
     puts "SETTING QUESTIONS ATTRIBUTES to #{attributes.inspect}"
     attributes.each do |k, v|
-      puts "#{k} / v: #{v.inspect}"
+      question_id = v["id"]
+
+      if v.key?("multi_choice_answer")
+        option_id = v["multi_choice_answer"]["multi_choice_option_id"]
+        MultiChoiceAnswer.create(multi_choice_option_id: option_id)
+      elsif v.key?("text_answer")
+        body = v["text_answer"]["body"]
+        text_style = Question.find(question_id).style
+        TextAnswer.create(body: body, text_style_id: text_style.id)
+      else
+        raise "UNKNOWN ANSWER TYPE"
+      end
     end
   end
 end
