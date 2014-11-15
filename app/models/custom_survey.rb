cd5f73bc88f0a58fccaefa0482a2bf71a5e3c2cb
Gabe Berke-Williams <gabebw@gabebw.com>
2014-11-15 18:34:01 -0500

    Debug exactly what's coming in

--- app/models/custom_survey.rb
+++ app/models/custom_survey.rb
@@ -7,5 +7,8 @@ class CustomSurvey
   # it silently doesn't grab questions.
   def questions_attributes=(attributes)
     puts "SETTING QUESTIONS ATTRIBUTES to #{attributes.inspect}"
+    attributes.each do |k, v|
+      puts "#{k} / v: #{v.inspect}"
+    end
   end
 end
