6bc987e3d83ebd391e06b8dbce538a1f155114e8
Gabe Berke-Williams <gabebw@gabebw.com>
2014-11-15 18:42:43 -0500

    Associate TextStyle and TextAnswer

--- app/models/text_answer.rb
+++ app/models/text_answer.rb
@@ -1,5 +1,6 @@
 class TextAnswer < ActiveRecord::Base
   validates :body, presence: true
 
+  belongs_to :text_style
   has_one :response, as: :answer
 end
--- app/models/text_style.rb
+++ app/models/text_style.rb
@@ -2,4 +2,5 @@ class TextStyle < ActiveRecord::Base
   validates :body, presence: true
 
   has_one :question, as: :style
+  has_many :text_answers
 end
